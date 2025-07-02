fb50fcb96f15b2ea0264bc5d57cafcd2ca754414
mik-tf <logismos@protonmail.ch>
2025-07-02 09:14:14 -0400

    fix error for lua

--- filters/book_structure.lua
+++ filters/book_structure.lua
@@ -11,14 +11,15 @@ function Header(el)
     if string.match(header_text, '^[Pp]art %d+:') then
       local part_title = string.gsub(header_text, '^[Pp]art %d+: *', '')
       -- Use \newpage to ensure parts start on a new page
-      return pandoc.RawBlock('tex', '\\newpage\\part{' .. part_title .. '}')
+      return pandoc.RawBlock('tex', '\\clearpage\\part{' .. part_title .. '}')
     end
   
     -- Check for "Chapter X: ..." pattern, case-insensitive
     if string.match(header_text, '^[Cc]hapter %d+:') then
       local chapter_title = string.gsub(header_text, '^[Cc]hapter %d+: *', '')
       -- The \chapter command in the 'book' class already starts a new page
-      return pandoc.RawBlock('tex', '\\chapter{' .. chapter_title .. '}')
+      -- Add \par to ensure we're not in the middle of a paragraph
+      return pandoc.RawBlock('tex', '\\par\\chapter{' .. chapter_title .. '}')
     end
   
     return el
